5b77caa42ca6e2bbd8ea1a33b4f2774d0e408f76
Ren <chunyu.ren@accenture.com>
2020-12-25 21:11:20 +0800

    sb

---
+++ prepare-tf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git clone https://github.com/GeromeRen/infoblox_test.git
+
+ls -ail && pwd
+
+cp infoblox-terraform-plan-apply.sh /sharedfolder/infoblox-terraform-plan-apply.sh
+
+chmod 777 /sharedfolder/infoblox-terraform-plan-apply.sh
+
--- sb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-git clone https://github.com/GeromeRen/infoblox_test.git
-
