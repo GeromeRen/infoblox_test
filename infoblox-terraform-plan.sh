15d3b985e31cf7762cef30fb8a5d6da0b2b6c06a
Ren <chunyu.ren@accenture.com>
2020-12-26 23:04:50 +0800

    test

--- infoblox-terraform-plan.sh
+++ infoblox-terraform-plan.sh
@@ -24,24 +24,35 @@ echo "[Debug] environment variable IPAM_PWD"
 printenv | grep IPAM_PWD
 
 #echo "[Debug] Start to run terraform init"
-#terraform init -plugin-dir "$HOME/.terraform.d/plugins/" -get-plugins=false
-#
-#echo "[Debug] Start to list terraform providers"
-#ls -ail .terraform/plugins/linux_amd64
-#terraform providers
-#
-#echo "[Debug] Start to run terraform plan"
-#terraform plan -input=false -var "infoblox_username=$IPAM_ID"  -var "infoblox_password=$IPAM_PWD" -out "infoblox_tfplan.tfplan"
-#
-#echo "[Debug] List current folder after running terraform plan"
-#ls -ail && pwd
-#
-#terraform show -json infoblox_tfplan.tfplan > infoblox_tfplan.json
-#
-#echo "[Debug] List current folder after running terraform show"
-#ls -ail && pwd
-#
-#cp infoblox_tfplan.json /sharedfolder/infoblox_tfplan.json
-#
-#echo "[Debug] List sharedfolder folder after copying infoblox_tfplan.json to /sharedfolder"
-#ls /sharedfolder
+#terraform init -plugin-dir "$HOME/.terraform.d/plugins/" -get-plugins=false -backend-config="storage_account_name=sa5064iactf"
+terraform plan \
+  -plugin-dir "$HOME/.terraform.d/plugins/" \
+  -get-plugins=false \
+  -backend-config="storage_account_name=sa5064iactf" \
+  -backend-config="container_name=$AZURERM_BACKEND_STORAGE_CONTAINER" \
+  -backend-config="key=$AZURERM_BACKEND_STORAGE_CONTAINER_BLOB_KEY"
+
+echo "[Debug] Start to list terraform providers"
+ls -ail .terraform/plugins/linux_amd64
+terraform providers
+
+echo "[Debug] Start to run terraform plan"
+
+if [[ -z "${CMDARGS_TFVARS}" ]]; then
+  terraform plan -input=false -var "infoblox_username=$IPAM_ID"  -var "infoblox_password=$IPAM_PWD" -out "infoblox_tfplan.tfplan" ${CMDARGS_TFVARS}
+else
+  terraform plan -input=false -var "infoblox_username=$IPAM_ID"  -var "infoblox_password=$IPAM_PWD" -out "infoblox_tfplan.tfplan" 
+fi
+
+echo "[Debug] List current folder after running terraform plan"
+ls -ail && pwd
+
+terraform show -json infoblox_tfplan.tfplan > infoblox_tfplan.json
+
+echo "[Debug] List current folder after running terraform show"
+ls -ail && pwd
+
+cp infoblox_tfplan.json /sharedfolder/infoblox-workspace/infoblox_tfplan.json
+
+echo "[Debug] List sharedfolder folder after copying infoblox_tfplan.json to /sharedfolder"
+ls /sharedfolder/infoblox-workspace
